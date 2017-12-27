ca63097e46a34905349d8d4083ee766100f82129
Maxime Daniel <root@maxux.net>
2017-12-27 14:47:30 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,2 @@
+# ssl-check-expire
+Simple script to check expiration date of SSL services
---
+++ check.py
@@ -0,0 +1,53 @@
+import socket
+import datetime
+import ssl
+
+class SSLExpireCheck:
+    def __init__(self, target, port=443):
+        self.target = target
+        self.port = port
+
+    def expiration(self):
+        ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
+
+        try:
+            context = ssl.create_default_context()
+
+            conn = context.wrap_socket(
+                socket.socket(socket.AF_INET),
+                server_hostname=self.target,
+            )
+
+            conn.connect((self.target, self.port))
+            ssl_info = conn.getpeercert()
+
+        except ssl.SSLError as e:
+            return None
+
+        return datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+
+if __name__ == '__main__':
+    websites = [
+        "arya.maxux.net",
+        "clea.maxux.net",
+        "cbseraing.be",
+        "irc.maxux.net",
+        "ssh.maxux.net",
+        "www.maxux.net",
+        "www.cbseraing.be"
+    ]
+
+    services = [
+        ("irc.maxux.net", 6697),
+    ]
+
+    print("== Websites ==")
+    for website in websites:
+        site = SSLExpireCheck(website)
+        print("%-20s: %s" % (website, site.expiration()))
+
+    print("")
+    print("== Services ==")
+    for service in services:
+        remote = SSLExpireCheck(service[0], service[1])
+        print("%-20s: %s" % (("%s [%s]" % (service[0], service[1]), site.expiration())))
